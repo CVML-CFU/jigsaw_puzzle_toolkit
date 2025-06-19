d6861ff7c6a7c7ab659fb91a9a108d2b18af306e
Marixor <m.khoroshiltseva@unive.it>
2025-06-19 17:00:51 +0200

    fixed new size

--- create.py
+++ create.py
@@ -17,15 +17,22 @@ Type 2R:
 """
 
 import os 
-import argparse 
+import argparse
+import shutil
 from puzzle import PuzzleType, Puzzle
 
 def main(args):
-    
-    puzzle = Puzzle(args.input, args.puzzle_type, args.output)
-    puzzle.load_input_data(crop_pieces=True)
+    input_path = os.path.join(args.input, 'data.json')
+    puzzle = Puzzle(input_path, args.puzzle_type, args.output)
+    #puzzle = Puzzle(args.input, args.puzzle_type, args.output)
+    puzzle.load_input_data(crop_pieces=True, new_size=args.new_size)
     # puzzle.create_pieces()
     puzzle.save()
+    # copy preview
+    shutil.copy2(os.path.join(args.input, 'adjacency_preview.png'),
+                 os.path.join(puzzle.output_dir, 'adjacency_preview.png'))
+    shutil.copy2(os.path.join(args.input, 'preview.png'),
+                 os.path.join(puzzle.output_dir, 'preview.png'))
     
     return 1
 
@@ -35,6 +42,7 @@ if __name__ == '__main__':
     parser.add_argument('--input', '-I', type=str, default='input', help='path to an image or a set of fragments')
     parser.add_argument('--puzzle_type', '-PT', type=PuzzleType, choices=list(PuzzleType), help='puzzle type')
     parser.add_argument('--output', '-O', type=str, default='output', help='path to the output where the puzzle files will be placed')
+    parser.add_argument('--new_size', '-S', type=int, default=251, help='new size for rescaling')
 
     args = parser.parse_args()
     main(args)
