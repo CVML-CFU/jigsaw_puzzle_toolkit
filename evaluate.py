fixed translation and rotation issue for RMSE

evaluate.py
@@ -9,12 +9,13 @@ import json
 
 def main():
 
-    #data_dir = "/run/user/1000/gvfs/sftp:host=gpu1.dsi.unive.it,user=luca.palmieri/home/ssd/datasets/RePAIR_ReLab_luca"
-    data_dir = "/run/user/1000/gvfs/sftp:host=gpu1.dsi.unive.it,user=m.khoroshiltseva/home/ssd/datasets/RePAIR_ReLab_marina/"
+    data_dir = "/run/user/1000/gvfs/sftp:host=gpu1.dsi.unive.it,user=luca.palmieri/home/ssd/datasets/RePAIR_ReLab_luca"
+    # data_dir = "/run/user/1000/gvfs/sftp:host=gpu1.dsi.unive.it,user=m.khoroshiltseva/home/ssd/datasets/RePAIR_ReLab_marina/"
     test_set = np.loadtxt(os.path.join(data_dir, 'PAD_v2', 'test.txt'), dtype=str)
     # test_set = test_set[10:]
     experiments_folder = os.path.join(data_dir, 'experiments')
     preprocessing_folder = os.path.join(data_dir, 'preprocessing')
+    preprocessing_folder_rescaling = "/run/user/1000/gvfs/sftp:host=gpu1.dsi.unive.it,user=luca.palmieri/home/ssd/datasets/RePAIR_ReLab_luca_resize"
     Q_pos_list = []
     xy_num_points_list = []
     theta_num_points_list = []
@@ -44,12 +45,15 @@ def main():
                         sol_path = os.path.join(test_puzzle_folder, run, folder, 'solution.txt')
                         if os.path.exists(sol_path):
                             print("solution found")
+
                             # solution as numpy array
                             solution = np.loadtxt(sol_path, dtype=str)
                             # gt as numpy array
                             ground_truth_xyz = np.loadtxt(os.path.join(preprocessing_folder, test_puzzle, 'ground_truth.txt'))
                             with open(os.path.join(preprocessing_folder, test_puzzle, 'ground_truth.json'), 'r') as jgtp:
                                 ground_truth_json = json.load(jgtp)
+                            with open(os.path.join(preprocessing_folder_rescaling, test_puzzle, 'puzzle_info.json'), 'r') as jpi:
+                                puzzle_info = json.load(jpi)
                             # Create the dictionaries with piece id as key and the values
                             results = {}
                             ground_truth = {}
@@ -74,43 +78,43 @@ def main():
                             # piece_path = os.path.join(preprocessing_folder, test_puzzle, 'images', f"{pid}.png")
                             # path_lists[pid] = piece_path                        
                             
-                        eval = Evaluation()
-                        ## Evaluate
-                        print("#" * 50)
-                        print(f"Evaluating run {run}, sol {folder} on {test_puzzle}")    
-                        avg_q_pos, avg_rmse_rot, avg_rmse_translation = eval.evaluate(pieces_names, results, ground_truth, path_lists, transform_matrix)
-                        ## print stats
+                            eval = Evaluation()
+                            ## Evaluate
+                            print("#" * 50)
+                            print(f"Evaluating run {run}, sol {folder} on {test_puzzle}")    
+                            avg_q_pos, avg_rmse_rot, avg_rmse_translation = eval.evaluate(pieces_names, results, ground_truth, path_lists, transform_matrix, puzzle_info)
+                            ## print stats
                                    
-                        print(f"Q_pos: {avg_q_pos:.03f}")
-                        print(f"RMSE_rot: {avg_rmse_rot:.03f}")
-                        print(f"RMSE_t: {avg_rmse_translation:.03f}")
-                        print("-" * 30)
-                        # print params so I understand what I am evaluating
-                        with open(os.path.join(test_puzzle_folder, run, folder, 'solution_output_params.yaml'), 'r') as yf:
-                            params = yaml.safe_load(yf)
-                        print("Parameters:")
-                        print(f"CM: \t{params['aggregation']['method']}")
-                        print(f"Solver:")
-                        if 'anchor_index' in params['solver'].keys():
-                            anchor_idx = params['solver']['anchor_index']
-                        elif 'anchor_idx' in params['solver'].keys():
-                            anchor_idx = params['solver']['anchor_idx']
-                        else:
-                            anchor_idx = params['input_params']['solver']['anchor_index']
-                        print(f"\tAnchor: {anchor_idx}")
-                        print(f"\tTmax: {params['solver']['T_max']}")                
-                        # print(f"\tPQ_mode: {params['solver']['PQ_mode']}")
-                        print("#" * 50)
+                            print(f"Q_pos: {avg_q_pos:.03f}")
+                            print(f"RMSE_rot: {avg_rmse_rot:.03f}")
+                            print(f"RMSE_t: {avg_rmse_translation:.03f}")
+                            print("-" * 30)
+                            # print params so I understand what I am evaluating
+                            with open(os.path.join(test_puzzle_folder, run, folder, 'solution_output_params.yaml'), 'r') as yf:
+                                params = yaml.safe_load(yf)
+                            print("Parameters:")
+                            print(f"CM: \t{params['aggregation']['method']}")
+                            print(f"Solver:")
+                            if 'anchor_index' in params['solver'].keys():
+                                anchor_idx = params['solver']['anchor_index']
+                            elif 'anchor_idx' in params['solver'].keys():
+                                anchor_idx = params['solver']['anchor_idx']
+                            else:
+                                anchor_idx = params['input_params']['solver']['anchor_index']
+                            print(f"\tAnchor: {anchor_idx}")
+                            print(f"\tTmax: {params['solver']['T_max']}")                
+                            # print(f"\tPQ_mode: {params['solver']['PQ_mode']}")
+                            print("#" * 50)
 
-                        Q_pos_list.append(avg_q_pos)
-                        RMSE_r_list.append(avg_rmse_rot)
-                        RMSE_t_list.append(avg_rmse_translation)
-                        CM_type_list.append(params['aggregation']['method'])
-                        xy_num_points_list.append(params['grid_params']['xy_num_points'])
-                        theta_num_points_list.append(params['grid_params']['theta_num_points'])
-                        no_rotations_list.append(params['solver']['no_rotations'])
-                        anchor_ids_list.append(anchor_idx)
-                        evaluated_test_set.append(test_puzzle)
+                            Q_pos_list.append(avg_q_pos)
+                            RMSE_r_list.append(avg_rmse_rot)
+                            RMSE_t_list.append(avg_rmse_translation)
+                            CM_type_list.append(params['aggregation']['method'])
+                            xy_num_points_list.append(params['grid_params']['xy_num_points'])
+                            theta_num_points_list.append(params['grid_params']['theta_num_points'])
+                            no_rotations_list.append(params['solver']['no_rotations'])
+                            anchor_ids_list.append(anchor_idx)
+                            evaluated_test_set.append(test_puzzle)
                 
     eval_df = pd.DataFrame()
     eval_df['puzzle'] = evaluated_test_set

evaluation.py
@@ -17,7 +17,7 @@ class Evaluation:
             self._img_cache[pid] = img
         return img
 
-    def evaluate(self, pieces, results, ground_truth, path_lists, transform_matrix):
+    def evaluate(self, pieces, results, ground_truth, path_lists, transform_matrix, puzzle_info):
         """
         pieces is a list of pieces to evaluate
         results and ground_truth are two dictionary with the following keys:
@@ -39,7 +39,7 @@ class Evaluation:
 
         q_pos = 0
         q_pos = self.calculate_q_pos(pieces, results, ground_truth, path_lists)
-        rmse_value = self.calculate_rmse(pieces, results, ground_truth, path_lists, transform_matrix)
+        rmse_value = self.calculate_rmse(pieces, results, ground_truth, path_lists, transform_matrix, puzzle_info)
 
         new_row = pd.DataFrame([{'object_name': 3, 'Q_pos': q_pos, 'RMSE_rot': rmse_value['RMSE_rot'],
                                  'RMSE_translation': rmse_value['RMSE_translation']}])
@@ -56,7 +56,7 @@ class Evaluation:
         # Placeholder for evaluation logic
         return avg_q_pos, avg_rmse_rot, avg_rmse_translation
 
-    def calculate_rmse(self, pieces, results, ground_truth, path_lists, transform_matrix):
+    def calculate_rmse(self, pieces, results, ground_truth, path_lists, transform_matrix, puzzle_info=None):
         # Load the CSV files into pandas DataFrames
         result_data = []
         gt_data = []
@@ -113,10 +113,24 @@ class Evaluation:
         T_gt[:,0] = merged_df['x_gt'].values
         T_gt[:,1] = merged_df['y_gt'].values
 
-        euclidean_distances = np.linalg.norm(T_estimated - T_gt, axis=1, ord=2)
-        euclidean_distances_in_mm = euclidean_distances * pxls_to_m_scale
+        euclidean_distances_in_RL = np.linalg.norm(T_estimated - T_gt, axis=1, ord=2)               # these are the distances in our "resized" images
+        if puzzle_info is not None:
+            euclidean_distances_in_px = euclidean_distances_in_RL * puzzle_info['rescaling_factor']     # these are distances in the pixel space (rendered one)
+        else:
+            euclidean_distances_in_px = euclidean_distances_in_RL
+        euclidean_distances_in_mm = euclidean_distances_in_px * pxls_to_m_scale                     # these are distances in millimeters (real values)
         # the RMSE error between the distances and zero (if the T_estimated are exactly the T_gt, dists are zero!)
         RMSE_t = root_mean_squared_error(euclidean_distances_in_mm, np.zeros((N-1,1)))
+
+        # print("*" * 50)
+        # print(f"Errors for {N-1} pieces")
+        # print(f"ED_RL: {euclidean_distances_in_RL}")
+        # print(f"ED_px: {euclidean_distances_in_px}")
+        # print(f"ED_mm: {euclidean_distances_in_mm}")
+        # print("-" * 50)
+        # print(f"RMSE (t): {RMSE_t}")
+        # print("*" * 50)
+        # breakpoint()
 
         # print("lib RMSE")
         # print(RMSE_t)

puzzle.py
@@ -239,8 +239,9 @@ class Puzzle:
             self.puzzle_info['binary_masks_available'] = True
             self.puzzle_info['polygons_available'] = True
             self.puzzle_info['ground_truth_available'] = True
-            self.puzzle_info['max_dist_from_center'] = int(self.max_enclosing_radius)
+            self.puzzle_info['max_dist_from_center'] = np.ceil(self.max_enclosing_radius).astype(float)
             self.puzzle_info['padding'] = self.padding
+            self.puzzle_info['rescaling_factor'] = rescaling_factor
             # self.show_piece(3)
             # breakpoint()
             # print(self.sizes)
@@ -422,24 +423,25 @@ class Puzzle:
         return cen_image, cen_bmask, cen_polygon, np.asarray([shift_x, shift_y])
 
     def save(self):
+        # breakpoint()
         print("saving..")
         self.output_dir = os.path.join(self.output_path, os.path.basename(os.path.dirname(os.path.join(self.input_path))))
         os.makedirs(self.output_dir, exist_ok=True)
-        images_out_dir = os.path.join(self.output_dir, 'images')
-        os.makedirs(images_out_dir, exist_ok=True)
-        bmasks_out_dir = os.path.join(self.output_dir, 'binary_masks')
-        os.makedirs(bmasks_out_dir, exist_ok=True)        
-        polygons_out_dir = os.path.join(self.output_dir, 'polygons')
-        os.makedirs(polygons_out_dir, exist_ok=True)
-        for frag_key in self.input_data.keys():
-            frag_data = self.input_data[frag_key]
+        # images_out_dir = os.path.join(self.output_dir, 'images')
+        # os.makedirs(images_out_dir, exist_ok=True)
+        # bmasks_out_dir = os.path.join(self.output_dir, 'binary_masks')
+        # os.makedirs(bmasks_out_dir, exist_ok=True)        
+        # polygons_out_dir = os.path.join(self.output_dir, 'polygons')
+        # os.makedirs(polygons_out_dir, exist_ok=True)
+        # for frag_key in self.input_data.keys():
+        #     frag_data = self.input_data[frag_key]
 
-            # for name, image, bmask, polygon in zip(self.names, self.images, self.masks, self.polygons):
-            plt.imsave(os.path.join(images_out_dir, f"{frag_data['idx']}_{frag_data['name']}.png"), frag_data['image'])
-            cv2.imwrite(os.path.join(bmasks_out_dir, f"{frag_data['idx']}_{frag_data['name']}.png"), frag_data['mask'])
-            np.save(os.path.join(polygons_out_dir, f"{frag_data['idx']}_{frag_data['name']}"), frag_data['polygon'])
+        #     # for name, image, bmask, polygon in zip(self.names, self.images, self.masks, self.polygons):
+        #     plt.imsave(os.path.join(images_out_dir, f"{frag_data['idx']}_{frag_data['name']}.png"), frag_data['image'])
+        #     cv2.imwrite(os.path.join(bmasks_out_dir, f"{frag_data['idx']}_{frag_data['name']}.png"), frag_data['mask'])
+        #     np.save(os.path.join(polygons_out_dir, f"{frag_data['idx']}_{frag_data['name']}"), frag_data['polygon'])
         
-        np.savetxt(os.path.join(self.output_dir, "ground_truth.txt"), np.asarray(self.positions))
+        # np.savetxt(os.path.join(self.output_dir, "ground_truth.txt"), np.asarray(self.positions))
         with open(os.path.join(self.output_dir, "ground_truth.json"), 'w') as jf:
             json.dump(self.gt, jf, indent=2)
         with open(os.path.join(self.output_dir, "puzzle_info.json"), 'w') as jf: